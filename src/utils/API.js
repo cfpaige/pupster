bc701848f7baa9c190846d0319e4e6f866c2f4c6
ninateng <niinateng@gmail.com>
2019-07-30 12:36:37 -0700

    update with working src .. our src is called srcOurs

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
-const BASEURL = "https://dog.ceo/dog-api/";
+// Export an object containing methods we'll use for accessing the Dog.Ceo API
 
-
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
-  search: function (query) {
-    return axios.get(BASEURL + query );
+  getRandomDog: function() {
+    return axios.get("https://dog.ceo/api/breeds/image/random");
+  },
+  getDogsOfBreed: function(breed) {
+    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
+  },
+  getBaseBreedsList: function() {
+    return axios.get("https://dog.ceo/api/breeds/list");
   }
 };
