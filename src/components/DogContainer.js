0242826622ba2df5c4ad9ceb124e66858714286c
cfpaige <cfpaige@gmail.com>
2019-07-30 11:39:02 -0700

    card files added

--- src/components/DogContainer.js
+++ src/components/DogContainer.js
@@ -4,5 +4,4 @@ import Row from "./Row";
 import Col from "./Col";
 import Card from "./Card";
 import SearchForm from "./SearchForm";
-import MovieDetail from "./MovieDetail";
 import API from "../utils/API";
